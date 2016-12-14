936cfbdd8ef165913a7e6c44215ca52861185b8c
Andrey <a.petrov@itransition.com>
2016-12-14 16:24:42 +0300

    Added config

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const request = require('request');
+const config = require('config');
 
 const app = express();
 
@@ -12,10 +13,10 @@ app.use(cookieParser());
 app.get('/api/badges/:part', function (req, res) {
 	const part = req.params['part'];
 	const project = req.query['key'];
-	const metric = req.query['metric']
-	const username = process.env.USER || '';
-	const password = process.env.PASS || '';
-	const sonarHost = process.env.HOST || '';
+	const metric = req.query['metric'];
+	const username = process.env.SONAR_USER || config.get('user');
+	const password = process.env.SONAR_PASS || config.get('password');
+	const sonarHost = process.env.SONAR_HOST || config.get('host');
 	const url = `http://${username}:${password}@${sonarHost}/api/badges/${part}?key=${project}&metric=${metric}`;
 
 	request({ url: url }, function (error, response, body) {
@@ -29,7 +30,7 @@ app.get('/api/badges/:part', function (req, res) {
 	});
 });
 
-app.listen(process.env.PORT || 8081, function () {
+app.listen(process.env.PORT || config.get('port'), function () {
 });
 
 module.exports = app;
